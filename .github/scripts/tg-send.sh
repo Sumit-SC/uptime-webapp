a6f2237075ec0057f99c18e1847d14adab44ace7
Sumit S. Chaure <43643943+Sumit-SC@users.noreply.github.com>
2026-05-08 15:54:54 +0530

    Refactor tg-send.sh to improve error handling

--- .github/scripts/tg-send.sh
+++ .github/scripts/tg-send.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
-send_tg() {
+set -e
 
-  local MESSAGE="$1"
+MESSAGE="$1"
 
-  PAYLOAD=$(jq -n \
-    --arg chat_id "$CHAT_ID" \
-    --arg text "$MESSAGE" \
-    --argjson thread_id "$THREAD_ID" \
-    '{
-      chat_id: $chat_id,
-      message_thread_id: $thread_id,
-      text: $text,
-      disable_web_page_preview: true
-    }')
+echo "Sending Telegram alert..."
 
-  curl -s -X POST \
-    "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
-    -H "Content-Type: application/json" \
-    -d "$PAYLOAD"
-}
+PAYLOAD=$(jq -n \
+  --arg chat_id "$CHAT_ID" \
+  --arg text "$MESSAGE" \
+  --argjson thread_id "$THREAD_ID" \
+  '{
+    chat_id: $chat_id,
+    message_thread_id: $thread_id,
+    text: $text,
+    disable_web_page_preview: true
+  }')
+
+curl -s -X POST \
+  "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+  -H "Content-Type: application/json" \
+  -d "$PAYLOAD"
+
+echo "Telegram alert sent."
